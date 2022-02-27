2951816487d2261a740b3f50c8a800c3a6966f7d
Romain Lécart <rlecart@student.42.fr>
2022-02-27 23:16:51 +0100

    adding/init sio to multiplayer feature

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,8 +3,18 @@ import { Col, Row } from 'reactstrap';
 import '../style/App.css';
 import '../style/ft_css/ft_css.css';
 import Display from './Display';
+import { io } from "socket.io-client";
 
 const App = ({ }) => {
+  const socket = React.useRef();
+
+  React.useEffect(() => {
+    socket.current = io("http://localhost:8000");
+    socket.current.on('connection', () => {
+      console.log('connected')
+    })
+  }, []);
+
   return (
     <Fragment>
       <div className='screen'>
